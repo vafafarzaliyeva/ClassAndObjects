58b5673a50e3a51582ba92a814b6ab3587dbdad9
vafafarzaliyeva <98798353+vafafarzaliyeva@users.noreply.github.com>
2022-02-20 23:19:53 +0300

    Add files via upload

---
+++ Task3/Task3/Book.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task3
+{
+    class Book
+    {
+        public string _name;
+        public int _pagecount;
+
+        public string Name {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.Length >= 3)
+                {
+                    _name = value;
+                }
+
+            } 
+        }
+
+        public int PageCount {
+            get
+            {
+                return _pagecount;
+            }
+
+            set
+            {
+                if (value >= 10)
+                {
+                    _pagecount = value;
+
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        public Book(string name,int count)
+        {
+            Name = name;
+            PageCount = count;
+        }
+    }
+}
---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Book book = new Book("Flowers for Algernon",100);
+            string [] Book = new string[3];
+            int elementsayi = 0;
+
+            while (true)
+            {
+                int i = 0; 
+                Console.WriteLine("Kitab adini daxil edin :");
+                string kitabadi = Console.ReadLine();
+                book.Name = kitabadi;
+                Console.WriteLine("Sehife sayini daxil edin");
+                string a = Console.ReadLine();
+                int say = int.Parse(a);
+                book.PageCount = say; 
+
+                if(book.PageCount==say && book.Name == kitabadi)
+                {
+                    Book[i] = kitabadi +" "+ say;
+                    i++;
+                    elementsayi++;
+                    if (elementsayi == Book.Length)
+                    {
+                        break;
+              
+                    }
+
+                   
+                
+                    
+              
+                }
+                else
+                {
+                    Console.WriteLine("Kriteriyalari desteklemir");
+                    
+                   
+                }
+                
+            }
+            foreach (string item in Book)
+                {
+                Console.WriteLine(item);
+                }
+
+               
+            }
+
+           
+            
+        
+                
+               
+                   
+
+
+                }
+            
+
+
+        }
+    
+

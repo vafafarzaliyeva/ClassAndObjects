1fa6263e99e180a4d34326b10e51cbef24c17fa7
vafafarzaliyeva <98798353+vafafarzaliyeva@users.noreply.github.com>
2022-02-20 23:09:43 +0300

    Add files via upload

---
+++ Task2/Task2/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    class Product
+    {
+        public string Name;
+        public int Price;
+        public int Count;
+        public int TotalIncome;
+
+
+        public int Sell(int say)
+        {
+            while (Count > 0)
+            {
+              
+             Count = Count - say;
+             TotalIncome += (Price * say);
+             break;      
+                
+               
+            }
+           
+            return TotalIncome; 
+           
+
+
+           
+
+        }
+        public Product(string name)
+        {
+            Name = name;
+        }
+
+
+
+    }
+}
+
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mehsul adi yazin");
+            Product product1 = new Product("Milla");
+          
+
+            product1.TotalIncome = 0;
+            product1.Count = 10;
+            product1.Price = 4;
+
+      
+
+            while (product1.Count>=1)
+            {
+            string ad = Console.ReadLine();
+
+                if (ad == product1.Name)
+                {
+                    while (true) { 
+                    Console.WriteLine($"Mehsuldan {product1.Count} ededdir.Nece eded satmaq isteyirsiniz ?");
+                    string a = Console.ReadLine();
+                    int say = int.Parse(a);
+                    Console.WriteLine($"Satildi.Umumi gelir = {product1.Sell(say)}"); 
+                        
+                    Console.WriteLine("Yene satmaq isteyirsinizmi? y/n");
+                    string b = Console.ReadLine();
+
+                       if (b == "y")
+                       {
+                           if (product1.Count == 1)
+                           {
+                            Console.WriteLine("Son mehsulu da satmaq isteyirsinizmi? y/n");
+                            string c = Console.ReadLine();
+
+                                   if (c == "y")
+                                   {
+                                     Console.WriteLine($"{product1.Sell(say)}");
+                                     break;
+
+                               
+                                   }
+                           else
+                           {
+                           break;
+                           }
+                       }
+                       else
+                       {
+                         break;
+                       }
+                           
+                            
+                          
+                        }
+                            else Console.WriteLine("Mehsuldan qalmayib");
+                            break;
+                          
+                  
+                       
+                    }
+                    
+;
+                }
+                else Console.WriteLine("Bele bir mehsul yoxdur");
+                
+            }
+
+            
+
+
+
+        }
+    }
+}

d2271b0b40d586549deef7d65f0f22070336f981
vafafarzaliyeva <vafahf@code.edu.az>
2022-02-20 22:30:58 +0300

    First Commit

---
+++ Task1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Istifadeci adinizi teyin edin");
+            User user = new User("Vefa");
+           
+           
+            while (true)
+            {
+                string a = Console.ReadLine();
+                
+                user.Username = a;
+                if (user.Username == a)
+                {
+                   
+                    Console.WriteLine("Sifrenizi teyin edin");
+                    while (true)
+                    {
+                        string b = Console.ReadLine();
+                        user.Password = b;
+
+                        if (user.Password == b)
+                        {
+                            Console.WriteLine("Qeydiyyat ugurla basa catdi");
+                            break;
+                        }
+                        
+                        else
+                        {
+                            Console.WriteLine("Duzgun sifre daxil edin.Minumum 8simvoldan ibaret olsun. 1 boyuk simvol , 1 kicik simvol ve 1 reqem olmalidir");
+                        }
+                    }
+                    break;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Minumum 8 simvoldan ibaret istifadeci adi daxil edin");
+                }
+                
+            }
+
+       
+         
+           
+
+
+           
+
+           
+            
+           
+         
+
+           
+        }
+    }
+}
---
+++ Task1/User.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1
+{
+    class User
+    {
+        private string _username;
+        private string _password;
+
+
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                
+
+                if (value.Length >= 8)
+                {
+                  _username = value;
+                }
+                
+                 
+           
+            }
+        }
+       
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+
+                int countLower = 0;
+                int countUpper = 0;
+                int countDigit = 0;
+
+                if (value.Length >= 8) { 
+                foreach (char item in value)
+                {
+                    if (char.IsLower(item))
+                    {
+                        countLower++;
+                    }
+                    else if (char.IsUpper(item))
+                    {
+                        countUpper++;
+                    }
+                    else if (char.IsDigit(item))
+                    {
+                        countDigit++;
+                    }
+
+                }
+                if (countLower >= 1 && countUpper >= 1 && countDigit >= 1)
+                {
+                    _password = value;
+
+                  
+                }
+                }
+            }
+
+        }
+       
+       
+        public User(string name)
+        {
+            _username = name;
+        }
+        
+
+
+
+
+
+        
+    }
+}
